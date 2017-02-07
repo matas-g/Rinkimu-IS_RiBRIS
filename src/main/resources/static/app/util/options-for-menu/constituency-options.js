48798847129ed04b61593bcf8c1c2fa6957b01e6
Lukas Trišauskas <trisauskas90@gmail.com>
2017-02-07 18:06:11 +0200

    branch copy

---
+++ src/main/resources/static/app/util/options-for-menu/constituency-options.js
@@ -0,0 +1,19 @@
+var servedDetails = [
+  {
+    img: '../../img/results.png',
+    title: 'Apygardos',
+    pathTo: '/constituencies/list'
+  },
+  {
+    img: '../../img/add.png',
+    title: 'Pridėti apygardą',
+    pathTo: '/constituencies/add'
+  },
+  {
+    img: '../../img/add.png',
+    title: 'Pridėti apygardų sarašą',
+    pathTo: '/constituencies/add-list'
+  }
+];
+
+module.exports = servedDetails;
