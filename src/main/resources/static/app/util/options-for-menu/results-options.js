fd9c9d5cb99088ee46449452419a0b3f2457665a
Lukas Trišauskas <trisauskas90@gmail.com>
2017-02-18 14:48:28 +0200

    Login modal added

--- src/main/resources/static/app/util/options-for-menu/results-options.js
+++ src/main/resources/static/app/util/options-for-menu/results-options.js
@@ -1,5 +1,12 @@
 var servedDetails = [
-
+  {
+    text: 'Registruoti vienmandačių ruzultatus',
+    pathTo: '/results/single/add'
+  },
+  {
+    text: 'Registruoti daugiamandačių rezultatus',
+    pathTo: '/results/multi/add'
+  }
 ];
 
 module.exports = servedDetails;
