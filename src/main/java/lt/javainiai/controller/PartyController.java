b9664e3e689d32b13ef17690ad64c6be2718ec0d
andexist <andexist@gmail.com>
2017-03-06 17:13:04 +0200

    No message

--- src/main/java/lt/javainiai/controller/PartyController.java
+++ src/main/java/lt/javainiai/controller/PartyController.java
@@ -7,7 +7,6 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
--- src/main/java/lt/javainiai/security/RepresentativeRepository.java
+++ src/main/java/lt/javainiai/security/RepresentativeRepository.java
@@ -1,4 +1,4 @@
-package lt.javainiai.repository;
+package lt.javainiai.security;
 
 import java.util.List;
 
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import lt.javainiai.model.RepresentativeEntity;
+import lt.javainiai.repository.RepositoryInterface;
 
 @Repository
 public class RepresentativeRepository implements RepositoryInterface<RepresentativeEntity> {
