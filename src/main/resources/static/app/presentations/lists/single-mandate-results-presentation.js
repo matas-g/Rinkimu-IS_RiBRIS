80031ddb6ddce5aa57a4c631c1b9fdfa8bf8b622
andexist <andexist@gmail.com>
2017-03-07 10:36:12 +0200

    Merge branch 'development' into my-development

--- src/main/resources/static/app/presentations/lists/single-mandate-results-presentation.js
+++ src/main/resources/static/app/presentations/lists/single-mandate-results-presentation.js
@@ -3,58 +3,60 @@ const NavLink = require('../navigation/nav-link');
 
 var SingleMandateComponent = React.createClass({
 	render: function() {
-		var nr = 1;
-		var ConstituenciesList = this.props.constituencies.map(function(constituency, index) {
-			return (
-				<tr key={index}>
-          			<td>{nr++}. {constituency.name}</td>
-				</tr>
-			);
-		});
-
 		return (
 
-				<div className="container-fluid col-md-10">
-					<div className="panel panel-default">
-						<div className="panel-heading"><strong>Balsavimo rezultatai vienmandatėse apygardose</strong></div>
-						<table className="table">
+						<table>
 							<thead>
 								<tr>
-									<th>Apygarda</th>
-									<th>
-										Apylinkių skaičius
-											<th>iš viso</th>  &nbsp;&nbsp;&nbsp;&nbsp;
-											<th>duomenis atsiuntė</th>
-									</th>
-									<th>Rinkėjų saičius</th>
-									<th>
-										Dalyvavo<br />
-										skaičius / %
-									</th>
-									<th>
-										Negaliojantys biuleteniai<br />
-										skaičius / %
-									</th>
-									<th>
-										Galiojantys biuleteniai<br />
-										skaičius / %
-									</th>
+									<th rowspan="2">State</th>
+									<th rowspan="2">Utilities Company</th>
+									<th colspan="3">Summer Period</th>
+								</tr>
+								<tr>
+									<th>data1</th>
+									<th>data2</th>
+									<th>data3</th>
 								</tr>
 							</thead>
 							<tbody>
 								<tr>
-									<td>1. Naujamiesčio</td>
-									<tr></tr>
-									<td>41696</td>
-									<td>28684 / 68,79</td>
-									<td>600 / 2,09</td>
-									<td>28084 / 97,91</td>
+									<td>State name</td>
+									<td>Utility co</td>
+									<td>Data for 1</td>
+									<td>Data for 2</td>
+									<td>Data for 3</td>
+								</tr>
+								<tr>
+									<td>State name</td>
+									<td>Utility co</td>
+									<td>Data for 1</td>
+									<td>Data for 2</td>
+									<td>Data for 3</td>
+								</tr>
+								<tr>
+									<td>State name</td>
+									<td>Utility co</td>
+									<td>Data for 1</td>
+									<td>Data for 2</td>
+									<td>Data for 3</td>
+								</tr>
+								<tr>
+									<td>State name</td>
+									<td>Utility co</td>
+									<td>Data for 1</td>
+									<td>Data for 2</td>
+									<td>Data for 3</td>
+								</tr>
+								<tr>
+									<td>State name</td>
+									<td>Utility co</td>
+									<td>Data for 1</td>
+									<td>Data for 2</td>
+									<td>Data for 3</td>
 								</tr>
 							</tbody>
 						</table>
-					</div>
-				</div>
-		
+					
 		);
 	}
 });
