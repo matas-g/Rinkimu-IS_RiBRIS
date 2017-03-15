3aa04314dff70d6aaa9ebf8c46658a80a06dacce
Lukas Trišauskas <trisauskas90@gmail.com>
2017-03-15 18:01:59 +0200

    chart

--- src/main/resources/static/app/presentations/lists/multi-mandate-results-presentation.js
+++ src/main/resources/static/app/presentations/lists/multi-mandate-results-presentation.js
@@ -1,9 +1,43 @@
 const React = require('react');
 const NavLink = require('../navigation/nav-link');
 const Link = require('react-router').Link;
+const BarChart = require("../../util/chart/chart");
 
 var MultiMandateComponent = React.createClass({
 		render: function() {
+			var chartData = {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+            label: '# of Votes',
+            data: [5, 6, 7, 3, 8, 2, 0 ,1],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    };
+		var options = {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    };
 
 		var nr = 1;
 		var self = this;
@@ -22,58 +56,56 @@ var MultiMandateComponent = React.createClass({
 						<td>{constituency.districtsWithResults}</td>
 					</tr>
 				);
-				
 			});
 
 			return (
 			<div>
-					
-			    <div className="container-fluid">
-					
+			  <div className="container-fluid">
 					<div className="form-group pull-right">
 						<input type="text" className="search form-control" placeholder="Ieškoti" onChange={this.props.onSearchTextChange} />
 					</div>
 						<h3>Balsavimo rezultatai daugiamandatėje apygardoje</h3>
 						<table className="table table-striped table-bordered">
 							<thead>
-								<tr className="table-head"> 
-									<th className="text-middle" style={{verticalAlign: 'middle'}} rowSpan="2">Partija, koalicija</th> 
-									<th colSpan="2">Apylinkių skaičius</th> 
-								</tr> 
-								<tr className="table-head"> 
-									<th>iš viso</th> 
+								<tr className="table-head">
+									<th className="text-middle" style={{verticalAlign: 'middle'}} rowSpan="2">Partija, koalicija</th>
+									<th colSpan="2">Apylinkių skaičius</th>
+								</tr>
+								<tr className="table-head">
+									<th>iš viso</th>
 									<th>duomenis atsiuntė</th>
 								</tr>
 								</thead>
-								<tbody> 
+								<tbody>
 									{ConstituenciesList}
-								</tbody> 
+								</tbody>
 						</table>
-				</div>			
+				</div>
+
+	      <BarChart chartData={chartData} options={options}/>
 
 				<div className="container-fluid">
-					
 					<div className="form-group pull-right">
 						<input type="text" className="search form-control" placeholder="Ieškoti" onChange={this.props.onSearchTextChange} />
 					</div>
 						<h3>Balsavimo rezultatai apygardose</h3>
 						<table className="table table-striped table-bordered">
 							<thead>
-								<tr className="table-head"> 
-									<th className="text-middle" style={{verticalAlign: 'middle'}} rowSpan="2">Apygardos</th> 
-									<th colSpan="2">Apylinkių skaičius</th> 
-								</tr> 
-								<tr className="table-head"> 
-									<th>iš viso</th> 
+								<tr className="table-head">
+									<th className="text-middle" style={{verticalAlign: 'middle'}} rowSpan="2">Apygardos</th>
+									<th colSpan="2">Apylinkių skaičius</th>
+								</tr>
+								<tr className="table-head">
+									<th>iš viso</th>
 									<th>duomenis atsiuntė</th>
 								</tr>
 								</thead>
-								<tbody> 
+								<tbody>
 									{ConstituenciesList}
-								</tbody> 
+								</tbody>
 						</table>
-				</div>	
-				
+				</div>
+
 			</div>
 			);
 		}
