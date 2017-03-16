b7a455c9c9899515bcd86fc3552df3f274ae3bf9
matas-g <matas.guzelis@gmail.com>
2017-03-16 06:01:41 +0200

    Fix

--- src/main/java/lt/javainiai/utils/MandatesRemainderComparator.java
+++ src/main/java/lt/javainiai/utils/MandatesRemainderComparator.java
@@ -1,17 +1,11 @@
 package lt.javainiai.utils;
 
 import java.util.Comparator;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-
-import lt.javainiai.model.PartyEntity;
-import lt.javainiai.service.CandidatesResultsSingleMandateService;
 
 public class MandatesRemainderComparator implements Comparator<WinnerPartyMultiMandate> {
 
-    @Autowired
-    private CandidatesResultsSingleMandateService candidatesResultsSingleMandateService;
+//    @Autowired
+//    private CandidatesResultsSingleMandateService candidatesResultsSingleMandateService;
 
     @Override
     public int compare(WinnerPartyMultiMandate party1, WinnerPartyMultiMandate party2) {
@@ -29,25 +23,25 @@ public class MandatesRemainderComparator implements Comparator<WinnerPartyMultiM
         }
 
         // 3 - number of single-member mandates won comparison
-        List<SingleMandateCandidateResults> singleMandateWinners = candidatesResultsSingleMandateService
-                .getWinnerCandidatesSingleMandate();
-        Long party1SingleMandates = 0L;
-        Long party2SingleMandates = 0L;
-
-        for (SingleMandateCandidateResults singleMandateWinner : singleMandateWinners) {
-            PartyEntity party = singleMandateWinner.getCandidate().getParty();
-
-            if (party.equals(party1.getParty())) {
-                party1SingleMandates++;
-            }
-            if (party.equals(party2.getParty())) {
-                party2SingleMandates++;
-            }
-        }
-        result = party2SingleMandates.compareTo(party1SingleMandates);
-        if (result != 0) {
-            return result;
-        }
+//        List<SingleMandateCandidateResults> singleMandateWinners = candidatesResultsSingleMandateService
+//                .getWinnerCandidatesSingleMandate();
+//        Long party1SingleMandates = 0L;
+//        Long party2SingleMandates = 0L;
+//
+//        for (SingleMandateCandidateResults singleMandateWinner : singleMandateWinners) {
+//            PartyEntity party = singleMandateWinner.getCandidate().getParty();
+//
+//            if (party.equals(party1.getParty())) {
+//                party1SingleMandates++;
+//            }
+//            if (party.equals(party2.getParty())) {
+//                party2SingleMandates++;
+//            }
+//        }
+//        result = party2SingleMandates.compareTo(party1SingleMandates);
+//        if (result != 0) {
+//            return result;
+//        }
 
         // 4 - party number comparison
         Long party1No = party1.getParty().getPartyNo();
